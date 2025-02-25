b980e7991e9d61ab528ea75201f6727ccad29326
이상협 <hyub0216@sk.com>
2025-02-25 10:25:40 +0900

    h2 DB

--- src/main/java/com/sktelecom/daisy/model/UserInfo.java
+++ src/main/java/com/sktelecom/daisy/model/UserInfo.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class UserInfo {
-    private Integer id;
+    private String userId;
     private String username;
     private String password;
     private String email;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO USER_INFO (userid,username, password, email) VALUES
+('P092740','john_doe', 'password123', 'john@example.com');
+
+INSERT INTO USER_INFO (userid,username, password, email) VALUES
+('P092741','jane_smith', 'password456', 'jane@example.com');
+
+INSERT INTO USER_INFO (userid,username, password, email) VALUES
+('P092742','alice_jones', 'password789', 'alice@example.com');
